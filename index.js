24502550dcd627ec7da7ff4d72179c72b8057794
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-02-19 18:44:02 -0700

    use port env var with heroku

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require("express")
-const helmet = require("helmet")
-const welcomeRouter = require("./welcome/welcome-router")
-const shoutsRouter = require("./shouts/shouts-router")
+const express = require("express");
+const helmet = require("helmet");
+const welcomeRouter = require("./welcome/welcome-router");
+const shoutsRouter = require("./shouts/shouts-router");
 
-const server = express()
-const port = 4000
+const server = express();
+const port = process.env.PORT || 4000;
 
-server.use(helmet())
-server.use(express.json())
-server.use("/", welcomeRouter)
-server.use("/shouts", shoutsRouter)
+server.use(helmet());
+server.use(express.json());
+server.use("/", welcomeRouter);
+server.use("/shouts", shoutsRouter);
 
 server.use((err, req, res, next) => {
-	console.log(err)
-	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
+ console.log(err);
+ res.status(500).json({
+  message: "Something went wrong"
+ });
+});
 
 server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+ console.log(`Server running at http://localhost:${port}`);
+});
